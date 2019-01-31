f0ed530978bbf494dcfe6d29c3a7325fc80f9dd9
KoingDev <sea.saingkoing@gmail.com>
2019-01-31 14:10:22 +0700

    Init

---
+++ Podfile
@@ -0,0 +1,10 @@
+inhibit_all_warnings!
+platform :ios, '9.0'
+
+target 'TestKDSwiftUtilities' do
+
+  use_frameworks!
+	
+	pod 'ReachabilitySwift'
+
+end
---
+++ TestKDSwiftUtilities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestKDSwiftUtilities
+//
+//  Created by KoingDev on 2019/01/31.
+//  Copyright © 2019 KoingDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
